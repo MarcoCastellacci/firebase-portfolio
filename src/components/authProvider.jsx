f151972119f2f80645d21db0af3491a083498c49
Marco Castellacci <marco.castellacci87@gmail.com>
2023-05-22 22:52:43 -0300

    Firebase Config Auth

---
+++ src/components/authProvider.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { Await, useNavigate } from "react-router-dom";
+import {
+
+    onAuthStateChanged,
+
+} from "firebase/auth";
+import { RegsiterNewUser, auth, userExist } from "../firebase/firebase";
+
+
+export default function AuthProvider({ children, onUserLoggedIn, onUserNotLoggedIn, onUserNotRegister }) {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                const isRegistered = await userExist(user.uid);
+                if (isRegistered) {
+                    navigate("/dashboard")
+                    onUserLoggedIn(user);
+                } else {
+                    await RegsiterNewUser({
+                        uid: user.uid,
+                        displayName: user.displayName,
+                        email: user.email,
+                        profilePicture: "",
+                        adminPermissed: false
+                    })
+                    onUserNotRegister(user);
+                    navigate("/signout")
+                }
+            } else {
+                onUserNotLoggedIn(user);
+                console.log("No hay Usuarios Coectados...");
+            }
+        });
+    }, [navigate, onUserLoggedIn, onUserNotLoggedIn, onUserNotRegister]);
+
+    return (
+        <>
+            <h1>{children}</h1>
+        </>
+    )
+}
