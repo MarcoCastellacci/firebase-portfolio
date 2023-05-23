2494de2c782f5a93a1a70c40fea8e016bedccfdb
Marco Castellacci <marco.castellacci87@gmail.com>
2023-05-22 22:50:55 -0300

    Firebase Config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,21 +1,96 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
+import {
+    initializeFirestore,
+    // eslint-disable-next-line
+    collection,
+    // eslint-disable-next-line
+    addDoc,
+    // eslint-disable-next-line
+    getDocs,
+    doc,
+    getDoc,
+    // eslint-disable-next-line
+    query,
+    // eslint-disable-next-line
+    where,
+    // eslint-disable-next-line
+    setDoc,
+    // eslint-disable-next-line
+    deleteDoc
+} from 'firebase/firestore';
+import {
+    getStorage,
+    // eslint-disable-next-line
+    ref,
+    // eslint-disable-next-line
+    uploadBytes,
+    // eslint-disable-next-line
+    getDownloadURL,
+    // eslint-disable-next-line
+    getBytes
+} from 'firebase/storage'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCdTFP7mZYBDUtOEVqoOn6FZ39lGHgQPCU",
-    authDomain: "portfolio-marco.firebaseapp.com",
-    projectId: "portfolio-marco",
-    storageBucket: "portfolio-marco.appspot.com",
-    messagingSenderId: "85303320263",
-    appId: "1:85303320263:web:ef1be831816e3dfdc828e2",
-    measurementId: "G-6WTYL0VC71"
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MESSUREMENTID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = initializeFirestore(app, {
+    experimentalAutoDetectLongPolling: true
+});
+export const storage = getStorage(app);
+
+export async function userExist(uid) {
+    try {
+        const docRef = doc(db, 'users', uid);
+        const res = await getDoc(docRef);
+        console.log(res);
+        return res.exists();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export async function existsUserName(username) {
+    const users = [];
+    const docsRef = collection(db, 'users')
+    const q = query(docsRef, where('username', '==', username));
+    const snapshot = await getDocs(q);
+
+    snapshot.forEach((doc) => {
+        users.push(doc.data());
+    });
+
+    return users.length > 0 ? users[0].uid : null;
+}
+
+export async function RegsiterNewUser(user){
+    try {
+        const collectionRef = collection(db, 'users')
+        const docRef = doc(collectionRef, user.uid)
+        await setDoc(docRef, user)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function UpdateUser(user){
+    try {
+        const collectionRef = collection(db, 'users')
+        const docRef = doc(collectionRef, user.uid)
+        await setDoc(docRef, user)
+    } catch (error) {
+        console.error(error);
+    }
+}
--- src/routes/loginView.jsx
+++ src/routes/loginView.jsx
@@ -1,10 +1,52 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+    GoogleAuthProvider,
+    signInWithPopup,
+} from "firebase/auth";
+import { auth, userExist } from "../firebase/firebase";
+import AuthProvider from "../components/authProvider";
 
+export default function LoginView() {
+    const navigate = useNavigate();
+    const [state, setState] = useState(0);
 
+    function handleUserLoggedIn(user) {
+        navigate('/dashboard')
+    }
+    function handleUserNotRegister(user) {
+        navigate('/')
+    }
+    function handleUserNotLoggedIn() {
+        setState(4)
+    }
 
-export default function LoginView(){
-return (
-<>
-<h1>Login View</h1>
-</>
-)
+//Funcion de Login Con Google
+    async function handleOnCLick() {
+        const googleProvider = new GoogleAuthProvider();
+        await SingInWithGoogle(googleProvider);
+
+        async function SingInWithGoogle(googleProvider) {
+            try {
+                const res = await signInWithPopup(auth, googleProvider);
+                console.log(res);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
+    if (state === 4) {
+        return (
+            <>
+                <button onClick={handleOnCLick}>Login View</button>
+            </>
+        );
+    }
+
+    return <AuthProvider
+        onUserLoggedIn={handleUserLoggedIn}
+        onUserNotRegister={handleUserNotRegister}
+        onUserNotLoggedIn={handleUserNotLoggedIn}>
+        <div>Cargando los Datos...</div>
+    </AuthProvider>
 }
